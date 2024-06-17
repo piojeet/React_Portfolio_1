548bf3123ed4078928ed7382f6d2ac84f0d6890f
piojeet <manjhipiojeet@gmail.com>
2024-06-18 00:48:02 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
-import React from 'react'
-import Header from './component/header/header'
-import Nav from './component/nav/nav'
-import About from './component/about/about'
-import Expriens from './component/expriens/expriens'
-import Services from './component/services/services'
-import Portfolio from './component/porfolio/porfolio'
-import Testimonial from './component/testimonial/testimonial'
-import Contact from './component/contact/contact'
-import Footer from './component/footer/footer'
+import React from 'react';
+import Header from './component/header/header';
+import Nav from './component/nav/nav';
+import About from './component/about/about';
+import Expriens from './component/expriens/expriens';
+import Services from './component/services/services';
+import Portfolio from './component/porfolio/porfolio';
+import Testimonial from './component/testimonial/testimonial';
+import Contact from './component/contact/contact';
+import Footer from './component/footer/footer';
 
 function App() {
   return (
--- src/component/testimonial/testimonial.jsx
+++ src/component/testimonial/testimonial.jsx
@@ -5,7 +5,16 @@ import AVTR2 from '../../asests/avatar2.jpg';
 import AVTR3 from '../../asests/avatar3.jpg';
 import AVTR4 from '../../asests/avatar4.jpg';
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 
 
@@ -38,17 +47,27 @@ const Testimonial = () => {
       <h5>Review from clients</h5>
       <h2>testimonial</h2>
 
-      <div className="container testimonial__container"
-      >
-        {data.map(({ avatar, name, review }, index) => (
-          <article key={index} className='testimonial'>
-            <div className="client__avatar">
-              <img src={avatar} alt="" />
-            </div>
-            <h5 className='client__name'>{name}</h5>
-            <small className='client__review'>{review}</small>
-          </article>
-        ))}
+   
+
+      <div className="container testimonial__container">
+      <Swiper 
+      // pagination={true}
+      grabCursor={true}
+        pagination={{
+          clickable: true,
+        }}
+       modules={[Pagination]}
+        className="mySwiper">
+      {data.map(({ avatar, name, review }, index) => (
+        <SwiperSlide key={index} className='testimonial'>
+          <div className="client__avatar">
+            <img src={avatar} alt="" />
+          </div>
+          <h5 className='client__name'>{name}</h5>
+          <small className='client__review'>{review}</small>
+        </SwiperSlide>
+      ))}
+      </Swiper>
       </div>
     </section>
   );
